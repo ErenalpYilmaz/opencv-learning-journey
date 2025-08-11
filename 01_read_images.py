fa662c0ddfe6f4dec64ea42ef6379a775aeb3bf8
ERENALP YILMAZ <erenalp@alpbilisim.com>
2025-08-11 21:47:39 +0300

    Update OpenCV basics: improve image read/write and add detailed comments
    - Refactor to use dynamic image paths with os module
    - Add function to save image to disk
    - Enhance code clarity with pixel format and image dimension explanations
    - Maintain image resizing and display functionality
    - Upgrade from previous simple read-resize-display example

--- 01_read_images.py
+++ 01_read_images.py
@@ -1,12 +1,68 @@
 import cv2 as cv
+import os
+"""
+    Pixel Range
+    8 bit representation
+    2^8 = 256
+    Min: 0 (Black)
+    Max: 255 (White)
+    
+    BGR Pixel
+    Blue - Green - Red [B, G, R]
+    
+    ----------------------------------
+    
+    Image Dimension
+    
+    Dim:(M, N, 3)
+    M - Height, y
+    N - Width, x
+"""
 
-# Reading image.
-img = cv.imread('images/woman.jpg')
+def readImage():
+    # Get the current working directory
+    root = os.getcwd()
+    
+    # Create the full path of the cat.jpg file in the image folder.
+    imgPath = os.path.join(root, 'images/cat.jpg')
+    
+    # Reading image.
+    img = cv.imread(imgPath)
+    
+    # Image resize
+    resized_img = cv.resize(img, (0,0), fx=0.5 , fy=0.5)
+    
+    #display images the new window
+    cv.imshow("Woman", resized_img)
+    
+    cv.waitKey(0)
 
-# Image resize
-resized_img = cv.resize(img, (0,0), fx=0.5 , fy=0.5)
+def writeImage():
+     # Get the current working directory
+    root = os.getcwd()
+    
+    # Create the full path of the cat.jpg file in the image folder.
+    imgPath = os.path.join(root, 'images/cat.jpg')
+    
+    # Reading image.
+    img = cv.imread(imgPath)
+    
+    #Create the full path of the output.jpg file in the image folder.
+    outPath = os.path.join(root, 'images/output.jpg')
 
-#display images the new window
-cv.imshow("Woman", resized_img)
+    #Write new images this path
+    cv.imwrite(outPath,img)
+    
+    
+if __name__ == "__main__":
+    readImage()
+    writeImage()
 
-cv.waitKey(0)
+
+
+
+
+
+
+
+
